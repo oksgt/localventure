04b0fba0138decb84cc6cf59f667a044daf78c94
Oka Sigit <oka.sigit9@gmail.com>
2025-04-27 19:03:04 +0700

    Fix navbar logo link to use route for admin home

--- resources/views/admin/partials/navbar.blade.php
+++ resources/views/admin/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center ">
-        <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('storage/assets/image/logo-utama.png') }}" class="mr-2"
+        <a class="navbar-brand brand-logo" href="{{ route('admin.home') }}"><img src="{{ asset('storage/assets/image/logo-utama.png') }}" class="mr-2"
                 alt="logo" /></a>
         <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('storage/assets/image/logo-utama.png') }}" alt="logo" /></a>
     </div>
